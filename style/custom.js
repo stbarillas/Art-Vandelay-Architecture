77a6eac62f8edc32bb1a59b1c93100a0225a3748
steve <stbarillas@gmail.com>
2019-02-19 23:44:35 -0800

    added slideshow logic

--- style/custom.js
+++ style/custom.js
@@ -1,13 +1,33 @@
-window.onload = function() {
-    let button = document.getElementById('button');
-    let modal = document.getElementById('modal');
-    let close = document.getElementById('close');
+let tile = document.getElementById('tile1');
+let modal = document.getElementById('modal');
+let close = document.getElementById('close');
+let html = document.getElementById('html');
 
-    button.onclick = function () {
-        modal.style.display = 'block';
-    }
+tile.onclick = function () {
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped')
 
-    close.onclick = function () {
-        modal.style.display = 'none';
-    }
+};
+
+close.onclick = function () {
+    modal.classList.remove('is-active');
+    html.classList.remove('is-clipped')
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("bayStreetSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
