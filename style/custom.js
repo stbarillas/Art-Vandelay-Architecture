65537fdc8f19a97a4aed838b48ab04ea10aac0b6
steve <stbarillas@gmail.com>
2019-02-20 00:41:07 -0800

    edited javascript functions to make them reusable for all tiles and modals

--- style/custom.js
+++ style/custom.js
@@ -1,29 +1,44 @@
-let tile = document.getElementById('tile1');
-let modal = document.getElementById('modal');
-let close = document.getElementById('close');
+// let tile = document.getElementById(tile);
+// let modal = document.getElementById(modal);
+// let close = document.getElementById(close_button);
 let html = document.getElementById('html');
+var myTileMapping = {
+    tile1: 'modal1',
+    close1: 'modal1'
+};
 
-tile.onclick = function () {
+var myModalMapping = {
+    tile1: 'bayStreetSlides',
+    arrow1a: 'bayStreetSlides',
+    arrow1b: 'bayStreetSlides'
+}
+
+var slideIndex = 1;
+
+function tile_click (tileId) {
+    let modal = document.getElementById(myTileMapping[tileId]);
     modal.classList.add('is-active');
     html.classList.add('is-clipped')
+    showDivs(slideIndex, tileId)
 
 };
 
-close.onclick = function () {
+function tile_close(tileId) {
+    let modal = document.getElementById(myTileMapping[tileId]);
     modal.classList.remove('is-active');
     html.classList.remove('is-clipped')
 }
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n, tileId) {
+  showDivs(slideIndex += n, tileId);
 }
 
-function showDivs(n) {
+function showDivs(n, tileId) {
   var i;
-  var x = document.getElementsByClassName("bayStreetSlides");
+  var x = document.getElementsByClassName(myModalMapping[tileId]);
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
